4bf72101041e721b0f5a088f922606d1f61895db
Kenneth Sullivan <90344298+kmsull@users.noreply.github.com>
2025-01-10 09:43:25 -0500

    Create infoCard.tsx
    added indicated component

---
+++ ksullivanportfolio/constants/infoCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../app/globals.css'
+
+export default InfoCard = () => {
+    return (
+        <div class="card">
+          <div class="first-content">
+            <span>First</span>
+          </div>
+          <div class="second-content">
+            <span>Second</span>
+          </div>
+        </div>
+    );
+};
