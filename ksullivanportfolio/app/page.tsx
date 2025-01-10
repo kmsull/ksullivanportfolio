d5921dd69726ef361f05f62f0ac89ddc3109519d
Kenneth <kmsullivanjr@uri.edu>
2025-01-09 21:07:25 -0500

    Basic changes

--- ksullivanportfolio/app/page.tsx
+++ ksullivanportfolio/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h1>Home</h1>
       <p>Welcome to my portfolio! I am a software engineer with a passion for front-end development. I have experience with React, Next.js, and TypeScript. 
         I am excited to continue to learn and grow as a developer. </p>
-      <Image src="/profile.jpg" alt="Picture of me" width={300} height={300} />
+      <Image href="../constants/images/profile.png" alt="Picture of me" width={300} height={300} />
     </div>
   );
 }
