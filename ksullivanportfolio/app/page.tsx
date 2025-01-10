b45965dbe7d6a547dbca0766917fda1916f7dbfb
Kenneth Sullivan <90344298+kmsull@users.noreply.github.com>
2025-01-10 08:54:54 -0500

    Update page.tsx
    fixed image sizing

--- ksullivanportfolio/app/page.tsx
+++ ksullivanportfolio/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import profilePicture from '../constants/images/profile.png'
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 
 export default function Home() {
   return (
@@ -8,7 +8,8 @@ export default function Home() {
       <div className="containerBorder px-[10%] py-[5%] flex flex-row  justify-content-center items-center">
       <Image src={profilePicture}
        alt="Picture of me"
-        className="rounded-full w-150 h-150 sm:w-50 sm:h-50 md:w-100 md:h-100" />
+        width={150} height={150}
+        className="rounded-full" />
         <div className="ml-[5%]">
           <h1 className="text-4xl "> Kenneth Sullivan </h1>
           <h1 className="mt-3 text-gray-400">Computer Science, BA</h1>
