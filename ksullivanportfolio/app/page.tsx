f02e442fd283edc43b42506304bc8012d198a9c8
Kenneth <kmsullivanjr@uri.edu>
2025-01-11 12:45:09 -0500

    added iinfo cards for display

--- ksullivanportfolio/app/page.tsx
+++ ksullivanportfolio/app/page.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Image from "next/image";
 import profilePicture from '../constants/images/profile.png'
+import InfoCard from "@/constants/infoCard";
 
 
 export default function Home() {
@@ -16,8 +18,10 @@ export default function Home() {
           <h1 className="mt-3 text-gray-400">University of Rhode Island</h1>
         </div>
       </div>
-        <div className="containerBorder px-40 py-20 flex flex-row  justify-content-center items-center">
-          
+        <div className="containerBorder px-40 py-20 flex flex-row space-x-8 place-content-center justify-content-center items-center">
+            <InfoCard title="Project 1" content="wow!"/>
+            <InfoCard title="Project 2" content="wow!"/>
+            <InfoCard title="Project 3" content="wow!"/>
         </div>
     </div>
   );
