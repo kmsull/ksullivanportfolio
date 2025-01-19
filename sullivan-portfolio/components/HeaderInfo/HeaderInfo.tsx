aa113b34f07d15b38e5e52531ea9e691bc30a95b
Kenneth <kmsullivanjr@uri.edu>
2025-01-19 11:37:02 -0500

    Header Styling

--- sullivan-portfolio/components/HeaderInfo/HeaderInfo.tsx
+++ sullivan-portfolio/components/HeaderInfo/HeaderInfo.tsx
@@ -7,11 +7,11 @@ import ResumeLink from "../SocialButton/ResumeLink";
 export default function HeaderInfo() {
   return (
     <div className="w-full h-[10%] rounded-lg p-[1%] border-4 border-gray-400">
-      <div className="flex flex-row justify-between items-center pl-[5%] pr-[5%]">
+      <div className="flex flex-row justify-center items-center space-x-[50%]">
         <div>
           <ResumeLink />
         </div>
-        <div className="flex flex-row space-x-[20%]">
+        <div className="flex flex-row space-x-[10%]">
           <InstagramButton />
           <LinkedInButton />
           <GithubButton />
