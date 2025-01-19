e4949ea7b787f2c3770b1eb93ac7a943577b9a3d
Kenneth <kmsullivanjr@uri.edu>
2025-01-18 19:13:37 -0500

    Cleaned up main page by implementing elements into their own component files

--- ksullivanportfolio/app/page.tsx
+++ ksullivanportfolio/app/page.tsx
@@ -1,28 +1,26 @@
 "use client";
+import HeaderInfo from "@/constants/HeaderInfo/HeaderInfo";
+import profilePicture from "../constants/images/profile.png";
 import Image from "next/image";
-import profilePicture from '../constants/images/profile.png'
-import InfoCard from "@/constants/infoCard";
-
 
 export default function Home() {
   return (
-    <div> 
-      <div className="containerBorder px-[10%] py-[5%] flex flex-row  justify-content-center items-center">
-      <Image src={profilePicture}
-       alt="Picture of me"
-        width={150} height={150}
-        className="rounded-full" />
-        <div className="ml-[5%]">
-          <h1 className="text-4xl "> Kenneth Sullivan </h1>
-          <h1 className="mt-3 text-gray-400">Computer Science, BA</h1>
-          <h1 className="mt-3 text-gray-400">University of Rhode Island</h1>
+    <div>
+      <HeaderInfo />
+      <div className="flex flex-row">
+        <div className="w-[10%]"></div>
+        <div className="containerBorder py-[5%] w-[80%] flex flex-row space-x-[20%] justify-center">
+          <div className="p-4 containerBorder">
+            <Image
+              src={profilePicture}
+              alt="Picture of me"
+              className="rounded-full"
+              width={250}
+            />
+          </div>
         </div>
+        <div className="w-[10%]"></div>
       </div>
-        <div className="containerBorder px-40 py-20 flex flex-row space-x-8 place-content-center justify-content-center items-center">
-            <InfoCard title="Project 1" content="wow!"/>
-            <InfoCard title="Project 2" content="wow!"/>
-            <InfoCard title="Project 3" content="wow!"/>
-        </div>
     </div>
   );
 }
