3cf1da9b3cb7f4b523a432a53759d51043833aee
Kenneth <kmsullivanjr@uri.edu>
2025-01-19 11:12:13 -0500

    Styled info card for main page

--- sullivan-portfolio/components/MyInfoCard/MyInfoCard.tsx
+++ sullivan-portfolio/components/MyInfoCard/MyInfoCard.tsx
@@ -5,31 +5,37 @@ import Image from "next/image";
 
 export default function MyInfoCard() {
   return (
-    <StyledWrapper>
-      <div className="MainCard">
-        <div className="CardMedia">
-          <Image src={profilePicture} alt="Placeholder" width={50} />
-        </div>
-
-        <div className="CardHeader">
-          <h2>Kenneth Sullivan</h2>
-        </div>
-
-        <div className="CardContent">
-          <p>
-            Computer Science graduate from the University of Rhode Island.
-            Passionate developer focused on innovative solutions and continuous
-            learning. Dedicated to mastering new technologies and taking on
-            complex challenges.
-          </p>
-        </div>
+    <div className="w-[50%] h-[100%] bg-gray-800 rounded-lg flex flex-col justify-center items-center text-white border-4 border-gray-400 m-[5%]">
+      <div className=" flex justify-center items-center">
+        <Image
+          src={profilePicture}
+          alt="Picture of the author"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: "25%", height: "auto" }}
+          className="rounded-full border-4 border-red-500 " // optional
+        />
       </div>
-    </StyledWrapper>
+
+      <div className=" text-center mt-[1%] mb-[1%] text-lg font-bold text-white ">
+        <h2>Kenneth Sullivan</h2>
+      </div>
+
+      <div className="text-center text-white text-md">
+        <p>
+          Computer Science graduate from the University of Rhode Island.
+          Passionate developer focused on innovative solutions and continuous
+          learning. Dedicated to mastering new technologies and taking on
+          complex challenges.
+        </p>
+      </div>
+    </div>
   );
 }
 
 const StyledWrapper = styled.div`
-  .MainCard {
+  .na {
     width: 100%;
     height: 100%;
     border-color: rgb(68, 68, 68);
