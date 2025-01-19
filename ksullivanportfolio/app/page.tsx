648984ac4c5a1ec37d9d31ea36879a6ea9e12564
Kenneth <kmsullivanjr@uri.edu>
2025-01-18 19:44:53 -0500

    Added the main info page card to the main page

--- ksullivanportfolio/app/page.tsx
+++ ksullivanportfolio/app/page.tsx
@@ -2,6 +2,7 @@
 import HeaderInfo from "@/constants/HeaderInfo/HeaderInfo";
 import profilePicture from "../constants/images/profile.png";
 import Image from "next/image";
+import MyInfoCard from "../constants/MyInfoCard/MyInfoCard";
 
 export default function Home() {
   return (
@@ -10,14 +11,7 @@ export default function Home() {
       <div className="flex flex-row">
         <div className="w-[10%]"></div>
         <div className="containerBorder py-[5%] w-[80%] flex flex-row space-x-[20%] justify-center">
-          <div className="p-4 containerBorder">
-            <Image
-              src={profilePicture}
-              alt="Picture of me"
-              className="rounded-full"
-              width={250}
-            />
-          </div>
+          <MyInfoCard />
         </div>
         <div className="w-[10%]"></div>
       </div>
