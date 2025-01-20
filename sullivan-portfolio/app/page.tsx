2c85665e5e48511961245cbd40885878cb555f79
Kenneth <kmsullivanjr@uri.edu>
2025-01-19 23:58:32 -0500

    Added a List of Interest and Languages Im proficient in

--- sullivan-portfolio/app/page.tsx
+++ sullivan-portfolio/app/page.tsx
@@ -1,18 +1,27 @@
 "use client";
 import HeaderInfo from "@/components/HeaderInfo/HeaderInfo";
 import MyInfoCard from "../components/MyInfoCard/MyInfoCard";
+import LanguageList from "../components/LanguageList/LanguageList";
+import InterestsList from "../components/InterestsLIst/InterestsList";
+import { ThemeProvider } from "next-themes";
 
 export default function Home() {
   return (
-    <div className=" h-screen">
-      <HeaderInfo />
-      <div className=" flex flex-row">
-        <div className="w-[15%] border-2 border-red-100"></div>
-        <div className="border-2 border-red-100 h-screen flex flex-col justify-center">
-          <MyInfoCard />
+    <ThemeProvider attribute="class">
+      <div className=" h-screen">
+        <HeaderInfo />
+        <div className=" flex flex-row">
+          <div className="w-[20%] border-2 border-red-100">
+            <InterestsList />
+          </div>
+          <div className="border-2 border-red-100 h-screen flex justify-center">
+            <MyInfoCard />
+          </div>
+          <div className="w-[20%] flex justify-center border-2 border-red-100">
+            <LanguageList />
+          </div>
         </div>
-        <div className="w-[15%] border-2 border-red-100"></div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
