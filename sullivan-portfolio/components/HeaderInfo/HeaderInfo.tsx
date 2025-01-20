bc59e7598dbe76e9f28e0621c662f411218592f4
Kenneth <kmsullivanjr@uri.edu>
2025-01-19 23:59:36 -0500

    Added switch to header

--- sullivan-portfolio/components/HeaderInfo/HeaderInfo.tsx
+++ sullivan-portfolio/components/HeaderInfo/HeaderInfo.tsx
@@ -3,11 +3,12 @@ import InstagramButton from "../SocialButton/InstagramButton";
 import LinkedInButton from "../SocialButton/LinkedInButton";
 import GithubButton from "../SocialButton/GithubButton";
 import ResumeLink from "../SocialButton/ResumeLink";
+import BB8Switch from "../LightDark/BB8Switch";
 
 export default function HeaderInfo() {
   return (
     <div className="w-full h-[10%] rounded-lg p-[1%] border-4 border-gray-400">
-      <div className="flex flex-row justify-center items-center space-x-[50%]">
+      <div className="flex flex-row justify-center items-center space-x-[20%]">
         <div>
           <ResumeLink />
         </div>
@@ -15,6 +16,9 @@ export default function HeaderInfo() {
           <InstagramButton />
           <LinkedInButton />
           <GithubButton />
+        </div>
+        <div>
+          <BB8Switch />
         </div>
       </div>
     </div>
